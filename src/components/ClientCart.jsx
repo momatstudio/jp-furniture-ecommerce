a5beaca804d85c91b1df936900c9d36728db021d
Mohau Matthews Maphalle <matthewsmohau@gmail.com>
2025-03-31 12:06:38 +0200

    enlarged the header icons

--- src/components/ClientCart.jsx
+++ src/components/ClientCart.jsx
@@ -7,7 +7,7 @@ export default function ClientCart() {
   const { cartItems } = useCart();
   return (
     <Link href="/cart" className="flex items-center text-black relative ml-4">
-      <CiShoppingCart size={22} className="hover:text-[#A42300] " />
+      <CiShoppingCart className="hover:text-[#A42300] text-[25px] md:text-[20px]" />
       {cartItems && cartItems.length > 0 && (
         <span className="absolute -top-1.5 -right-2 bg-[#A42300] text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
           {cartItems.length}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { companyInfo } from "../../data";
 
 import ClientLoginButton from "@/components/ClientLoginButton";
 import ClientCart from "@/components/ClientCart";
-import { CiMenuBurger, CiSearch } from "react-icons/ci";
+import { CiMenuBurger, CiPhone, CiSearch } from "react-icons/ci";
 import { BsPhone } from "react-icons/bs";
 
 export default function Header() {
@@ -24,7 +24,7 @@ export default function Header() {
           </li>
         </ul>
       </div> */}
-      <div className="flex items-center justify-between h-28 bg-white p-4 md:px-[15%] ">
+      <div className="flex items-center justify-between h-[85px] bg-white p-4 md:px-[15%] ">
         {/* <ul className="hidden md:flex space-x-4  text-sm font-[300]">
           <li className="flex items-center h-26 hover:text-[#A42300] cursor-pointer hover:underline">
             <Link href="/">
@@ -49,8 +49,9 @@ export default function Header() {
         </ul> */}
         <Link href={`tel:${companyInfo.phone}`}>
           <div className="flex items-center font-[200] text-black">
-            <BsPhone size={20} className="mr-2 hover:text-[#A42300]" />
+            <CiPhone className="mr-2 hover:text-[#A42300] text-[25px] md:text-[20px]" />
             <span className="hidden md:block">{companyInfo.phone}</span>
+            <span className=" md:hidden">CALL US</span>
           </div>
         </Link>
 
@@ -58,8 +59,8 @@ export default function Header() {
           <Link href="/">
             <Image
               src={"/logo1.svg"}
-              height={70 / 1.3}
-              width={143 / 1.3}
+              height={70 / 1.6}
+              width={143 / 1.6}
               alt={"JP Furniture logo"}
               className=" cursor-pointer rounded-full"
             />
@@ -74,7 +75,7 @@ export default function Header() {
           <ClientLoginButton />
           <ClientCart />
           <div className=" flex ml-6">
-            <CiMenuBurger size={22} />
+            <CiMenuBurger className="text-[25px] md:text-[20px]" />
             <span className="hidden md:block ml-2">MENU</span>
           </div>
         </div>
