5e5539bad85ba8492b5be05055a2613f0ddd8814
Mohau Matthews Maphalle <matthewsmohau@gmail.com>
2025-04-01 10:57:24 +0200

    fix environment issues

--- src/lib/auth0.js
+++ src/lib/auth0.js
@@ -1,7 +1,7 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
 export const auth0 = initAuth0({
-  baseURL: process.env.APP_BASE_URL,
+  baseURL: process.env.AUTH0_BASE_URL,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
